9a072c91455533e55157c85472eeb4d29a48b403
Trancefiend <33793523+Trancefiend@users.noreply.github.com>
2018-01-17 12:41:38 -0500

    Add files via upload

--- Joins/Join 4.sql
+++ Joins/Join 4.sql
@@ -1,23 +1,11 @@
-WITH raining AS(
-	SELECT Date
-	FROM weather
-	WHERE Events = 'Rain'
-	GROUP BY 1
-),
-
-rain_trips AS(
-	SELECT 
-		trip_id,
-		duration,
-		start_date
-	FROM trips
-	JOIN raining
-	ON  trips.start_date = raining.date
-	ORDER BY duration DESC 
-	)
-
 SELECT 
-	start_date,
-	duration
-FROM rain_trips
-GROUP BY 1
+	date(trips.start_date) trip_date,
+	max(trips.duration),
+	weather.Events
+FROM 
+	trips
+JOIN 
+	weather
+ON trip_date = weather.Date
+WHERE weather.Events = 'Rain'
+GROUP BY trip_date
